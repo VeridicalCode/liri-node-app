6953da4b75b7adc88497eb68a530c094cb01f122
quething <m.getsoian@gmail.com>
2019-09-26 20:33:46 -0700

    moment.js fix

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 
-// includes: env & file for masking keys, inquirer for prompts, spotify API for spotify, axios for other APIs
+// includes: env & file for masking keys, inquirer for prompts, spotify API for spotify, axios for other APIs, moment for date fixing
 require("dotenv").config();
 const inquirer = require("inquirer");
 const axios = require("axios");
@@ -7,9 +7,12 @@ const fs = require("fs");
 const keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
+const moment = require('moment');
 
+// function to look at user input and figure out which function to call.
+//   we separate this out from the prompt promise because we also want to be able to //   call it on the contents of random.txt if random.txt was queried
 const parseInputString = function (userString) {
-	let searchKey = "";
+	let searchKey = ""; // variable is a string, for safety
 	if (userString.startsWith('concert-this')) {
 		searchKey = userString.slice(13);
 		concertSearch(searchKey); // call concert function
@@ -25,7 +28,7 @@ const parseInputString = function (userString) {
 	else // if not a valid prompt, error out
 	{
 		console.log(`Sorry, I didn't understand that input. Try again?`);
-		promptUserAction();
+		promptUserAction(); // call the parent function with prompt/promise
 	}
 }
 
@@ -34,15 +37,15 @@ const promptUserAction = function () {
 	// give instructions
 	console.log(`\n'concert-this <artist name>' to find nearby concerts \n'spotify-this-song <song name>' to find album and artist information on a song \n'movie-this <movie>' to get IMDB information about a movie \n'do-what-it-says' to run the contents of the included text file \nYou may also type 'exit' to quit.`)
 
-	inquirer.prompt([
+	inquirer.prompt([ // use inquirer module to ask user for a string
 		{
 			type: "input",
 			name: "userInput"
 		}
-	]).then(
+	]).then( // fires on the user's input
 		function (inquirerResponse) {
-			let userString = inquirerResponse.userInput;
-			userString = userString.toLowerCase();
+			let userString = inquirerResponse.userInput; // store user input as var
+			userString = userString.toLowerCase(); // unify case first
 
 			if (userString.startsWith('do-what-it-says')) {
 				fireRandom(); // call random.txt
@@ -50,12 +53,20 @@ const promptUserAction = function () {
 			else if (userString.startsWith('exit')) {
 				return;
 			}
-			else {
+			else { // if we didn't quit or call the text file, operate on the command itself
 				parseInputString(userString);
 			}
 		});
 }
 
+// date-fixing function, using moment.js to make dates human-readable
+const fixDate = function(dateVar){
+	return moment(dateVar).format('MMMM Do YYYY, h:mm a');
+}
+
+
+// bands in town concert function. for some reason this doesn't always return the
+//  same results as pasting literally the exact same url into a web browser.
 const concertSearch = function (searchKey) {
 	console.log(`Searching for nearby concerts...\n`);
 	let urlVar = `https://rest.bandsintown.com/artists/${searchKey}/events?app_id=codingbootcamp`
@@ -63,21 +74,26 @@ const concertSearch = function (searchKey) {
 		.then(function (response) {
 			let bandJSON = response.data;
 
-			if (bandJSON.errormessage || bandJSON == null) {
-				console.log(`Something went wrong: ${bandJSON.errormessage}`);
+			if (!bandJSON[0].venue.city) { // bad data return is variable, but will at least always not have this property
+				console.log(`Something went wrong. That band may not be touring right now, or may not be a real band.`);
 			}
-			else {
+			else { // log to console/.txt file. We put these logs outside the FOR loop because we only want the header once
 				console.log(`Upcoming ${searchKey} concerts:`);
 				fs.appendFile('log.txt', `\n---- Upcoming ${searchKey} concerts: ----`, function (error) {
 					if (error) {
 						console.log(`AppendFile error: ${error}`);
 					}
-				})
+				}) // now we loop through the returned JSON and print every concert
 				for (let i = 0; i < bandJSON.length; i++) {
+					// fix the date
+					let playDate = fixDate(bandJSON[i].datetime);
+
+					// make a variable holding relevant data in a string
 					let resultString = `\n${bandJSON[i].venue.name}`
 					+ `\n${bandJSON[i].venue.city}`
-					+ `\n${bandJSON[i].datetime}`; //use moment to make that mm/dd/yy
+					+ `\n${playDate}`;
 
+					// log to console and log.txt
 					console.log(resultString);
 					fs.appendFile('log.txt', resultString, function (error) {
 						if (error) {
@@ -85,22 +101,26 @@ const concertSearch = function (searchKey) {
 						}
 					})
 				}
-			}
+			} // once we're done, loop back to user prompt again
 			promptUserAction();
 		});
 }
 
+// movie searching function
 const movieSearch = function (searchKey) {
 	console.log(`Searching for movie data...\n`);
+	// fallback search if movie string is null
 	if (!searchKey){
-		console.log(`No valid search key provided! Searching for a van Dormael classic.\n`)
+		console.log(`No valid search key provided! Searching for a van Dormael classic.\n`);
 		searchKey = 'Mr. Nobody';
 	}
 
+	// create valid url based on user input/fallback and feed to API via axios
 	let urlVar = `http://www.omdbapi.com/?apikey=trilogy&t=${searchKey}`
 	axios.get(urlVar)
 		.then(function (response) {
-			let movieJSON = response.data;
+			let movieJSON = response.data; // store response JSON as var
+			//pull relevant data, save to string
 			let resultString = `---- ${movieJSON.Title.toUpperCase()}`
 			+ `----\nYear: ${movieJSON.Year}`
 			+`\nCritical ratings: ${movieJSON.Ratings[0].Value} @IMDB, ${movieJSON.Ratings[1].Value} @Rotten Tomatoes`
@@ -108,57 +128,65 @@ const movieSearch = function (searchKey) {
 			+`\nStarring ${movieJSON.Actors}`
 			+`\n\n${movieJSON.Plot}`;
 
+			// print string to console & log.txt
 			console.log(resultString);
 			fs.appendFile('log.txt', `\n\n${resultString}`, function (error) {
 				if (error) {
 					console.log(`AppendFile error: ${error}`);
 				}
-			})
+			}) // loop back to user prompt
 			promptUserAction();
 		});
 }
 
+// function for spotify search on song
 const spotifySearch = function (searchKey) {
 	console.log(`Searching for song data...\n`);
+	// fallback search if song name is null
 	if (!searchKey){
 		console.log(`No valid search key was provided. Searching Ace of Base catalog instead.\n`);
 		searchKey = '"the sign"';
 	}
 
+	// unique spotify API doesn't use axios and can just take search key straight up
 	spotify.search({
 		type: 'track',
 		query: searchKey
 		}).then(function(response) {
 
-
+			// returned data is a bit of a mess; we want to drill down on the first entry in the first key
 			let songJSON = response.tracks.items[0];
 			
+			// pull out relevant data and store it as a string
 			let resultString = `---- ${songJSON.name.toUpperCase()} ----`
 					+`\nArtist: ${songJSON.artists[0].name}`
 					+`\nAlbum: ${songJSON.album.name}`
 					+`\nPreview on Spotify: ${songJSON.external_urls.spotify}`
+
+					// print to console and log.txt
 					console.log(resultString);
 					fs.appendFile('log.txt', `\n\n${resultString}`, 'utf8', function(error){
 						if (error){
 							console.log(`AppendFile error: ${error}`);
 						}
-					});
+					}); // loop back to user prompt
 			promptUserAction();
 			
-		}).catch(function(error) {
+		}).catch(function(error) { // spotify API provides an error throw, might as well catch it
     console.log(`Error in Spotify API: ${error}`);
   })
 
 }
 
+// function for reading the random.txt file
 const fireRandom = function () {
 	console.log(`Checking for external commands...`);
-	fs.readFile('./random.txt', 'utf8', (error, data) => {
+	fs.readFile('./random.txt', 'utf8', (error, contents) => {
 		if (error) {
 			console.log(`readFile error: ${error}`);
 		}
-		parseInputString(data);
+		parseInputString(contents); // whatever we get back, feed it to the actual parse function. if it's not valid parseInputString() will catch that.
 	});
 }
-
+// fire the parent prompt function once to get things going
 promptUserAction();
